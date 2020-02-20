57d695998c064d3f2a1b95f486e13cf194ddafa7
Yaz <yasmineoshaugh@gmail.com>
2020-02-20 15:16:46 -0800

    added check for fulfillment_status for all legal statuses.

--- lib/order.rb
+++ lib/order.rb
@@ -2,11 +2,15 @@ class Order
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
 
-  def initialize(id, products, customer, fulfillment_status)
+  def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = products
     @customer = customer
-    @fulfillment_status = fulfillment_status
+    if [:pending, :paid, :processing, :shipped, :complete].include? fulfillment_status
+      @fulfillment_status = fulfillment_status
+    else 
+      raise ArgumentError.new("That is not a valid fulfillment status.")
+    end
   end
 
   def total
