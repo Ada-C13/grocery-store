d116e67b0306a87230a38195509ccfae457bdf11
Lola Llanes <mslourdesllanes@gmail.com>
2020-02-20 14:48:48 -0800

    made customer class

---
+++ lib/customer.rb
@@ -0,0 +1,27 @@
+class Customer
+
+    attr_reader :ID
+    attr_accessor :email_address, :delivery_address
+
+
+    def initalize(ID, email_address, delivery_Address)
+        @ID = ID
+        @email_address = email_address
+        @delivery_address = delivery_address
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
