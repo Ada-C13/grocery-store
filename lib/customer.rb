ff684af4f4f0cf3c4e5949beb1a7a2129a172a14
Katie <katie.vandervoort@gmail.com>
2020-02-20 15:54:38 -0800

    updated customer.rb to pass wave 2

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,4 +1,4 @@
-# require_relative 'order'
+require 'csv'
 
 class Customer
   
@@ -11,4 +11,29 @@ class Customer
     @address = address # a hash
   end
 
+  def self.all
+    collection_of_customers = []
+    CSV.read("data/customers.csv").map(&:to_a).each do |customer|
+      collection_of_customers.push(Customer.new(customer[0].to_i,customer[1],{street: customer[2], city: customer[3], state: customer[4], zip: customer[5]}))
+    end
+    # print collection_of_customers[0].email
+    # print "\n"
+    return collection_of_customers
+  end
+# self.find(id) - returns an instance of Customer where the value of the id field in the CSV matches the passed parameter
+# Customer.find should not parse the CSV file itself. 
+# Instead it should invoke Customer.all and search through the results for a customer with a matching ID
+
+  def self.find(id)
+    # self.all.each do |customer|
+    #   if customer.id == id
+    #     return customer
+    #   end
+    # end
+    # return nil
+    return self.all.find { |customer| customer.id == id }
+  end
+
 end
+
+# puts "#{Customer.find(3).email}"
