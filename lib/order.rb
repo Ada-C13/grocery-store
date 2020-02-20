8ca1b75717e56b312bf86ce0ca5415b59abb9498
Lola Llanes <mslourdesllanes@gmail.com>
2020-02-20 14:50:21 -0800

    made order class

---
+++ lib/order.rb
@@ -0,0 +1,24 @@
+class Order
+
+    attr_reader :ID
+    
+    @Customer =
+    @ID = #int 
+    @Email =  #str
+    @fullfillment_status = 
+    @products = {} #hash Zero products is permitted (an empty hash) &there is only one of each product
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
