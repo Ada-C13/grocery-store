1c87e0f387072869b17565b0aee529ac91c297e1
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-02-20 14:27:45 -0800

    Added customer class and initialize and passed the test.

---
+++ lib/customer.rb
@@ -0,0 +1,14 @@
+#Wave 1
+# Create a class called Customer
+
+class Customer
+  attr_reader :id 
+  attr_accessor :email, :address
+  
+  def initialize(id, email, address)
+    @id = id
+    @email = email
+    @address = address
+  end
+
+end
