31a60d304c4d96abfb327f74c0db2f69dcc70cf4
Corinna Fabre <corinna.fabre@gmail.com>
2020-02-21 10:14:20 -0800

    Updated file with passing Customer.all method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'smarter_csv'
+
 class Customer
 
   attr_reader :id
@@ -8,4 +10,32 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    customers = []
+    CSV.open('data/customers.csv', 'r').each do |line|
+      read_customer = Customer.new(line[0].to_i, line[1], {:street => line[2], :city => line[3], :state => line[4], :zip => line[5]})
+      customers.push(read_customer)
+
+    end
+    return customers
+  end
+  #return customers
+
+  def self.find(id)
+    
+  end
 end
+
+# customers = SmarterCSV.process(
+#   '../data/customers.csv',
+#   {
+#     headers_in_file: false,
+#     user_provided_headers: %i[id email street city state zip],
+#     remove_empty_values: false,
+#     remove_zero_values: false,
+#   }
+# )
+
+# p customers.first
+# p customers.last
