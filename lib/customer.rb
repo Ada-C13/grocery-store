58d7b3cd70da6c97871ad605804b01aa266cbdfc
Yieni <yaynee8d@gmail.com>
2020-02-21 15:36:00 -0800

    functional code through wave 2 w/ optional for wave 2

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,7 @@
+require 'csv'
+
+
+
 class Customer
  attr_reader :id
  attr_accessor :email, :address
@@ -8,5 +12,33 @@ class Customer
     @address = address
   end
 
-end
+  def self.all
+    customer_data = CSV.read('data/customers.csv')
+    all_customers = Array.new
+    customer_data.each do |customer|
+      address = Hash.new
+      id = customer[0].to_i
+      email = customer[1].to_s
+      address[:street] = customer[2]
+      address[:city] = customer[3]
+      address[:state] = customer[4]
+      address[:zip] = customer[5]
+      customer_from_csv = Customer.new(id, email, address)
+      all_customers << customer_from_csv
+    end
+    return all_customers
+  end
+ 
+  def self.find(id)
+    all_customers = Customer.all
+    customer_w_id = 0
+    all_customers.each do |customer|
+      if customer.id == id
+        customer_w_id = customer
+      end
+    end
+    
+    return customer_w_id unless customer_w_id == 0
+  end
 
+end
--- lib/order.rb
+++ lib/order.rb
@@ -35,4 +35,61 @@ class Order
     return @products
   end
 
+  def remove_product(product_name)
+    if products.keys.include?(product_name)
+      products.delete(product_name)
+    else
+      raise ArgumentError
+    end
+  end
+
+  def self.all
+    order_data = CSV.read('data/orders.csv')
+    all_orders = Array.new
+
+    order_data.each do |order|
+      id = order[0].to_i
+
+      products = Hash.new
+      separate_order = order[1].split(";")
+      separate_order.each do |indiv_prod|
+        indiv_prod = indiv_prod.split(":")
+        products[indiv_prod[0]] = indiv_prod[1].to_f
+      end
+
+      customer = Customer.find(order[2].to_i)
+      fulfillment_status = order[3].to_sym
+      
+      order_from_csv = Order.new(id, products, customer, fulfillment_status)
+      all_orders << order_from_csv
+    end
+    return all_orders
+  end
+
+  def self.find(id)
+    all_orders = Order.all
+    order_w_id = 0
+    all_orders.each do |order|
+      if order.id == id
+        order_w_id = order
+      end
+    end
+    
+    return order_w_id unless order_w_id == 0
+  end
+
+  def self.find_by_customer(customer_id)
+    order_w_customer_id = 0
+    all_order_from_customer = Array.new
+    self.all.each do |order|
+      if order.customer.id == customer_id
+        order_w_customer_id = order
+        all_order_from_customer << order_w_customer_id
+      end
+    end
+    
+    return all_order_from_customer 
+  end
 end
+
+puts Order.find_by_customer(25)
