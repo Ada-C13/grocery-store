73604107634393da6c35522f33747217f30d93bd
Jessica Stone <jstone4815@gmail.com>
2020-02-21 15:21:42 -0800

    pass tests for self.all method

--- lib/order.rb
+++ lib/order.rb
@@ -29,4 +29,30 @@ class Order
       @products[name] = price
     end
   end
+
+  def self.all
+    all_orders = []
+
+    CSV.read('data/orders.csv').each do |order|
+      products_hash = {}
+  
+      id = order[0].to_i
+      products = order[1].split(';')
+      products.each do |product|
+        products_array = product.split(':')
+        products_hash[products_array[0]] = products_array[1].to_f
+      end
+
+      customer = Customer.find(order[2].to_i)
+      status = order[3].to_sym
+
+      all_orders << Order.new(id, products_hash, customer, status)
+    end
+    
+    return all_orders
+  end
+
+  def self.find(id)
+
+  end
 end
