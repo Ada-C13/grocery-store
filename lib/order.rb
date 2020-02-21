43f019c2b385325faa597cef6db1dfb8a3c1fe00
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-02-20 21:36:59 -0800

    passed 3 test in wave 1

--- lib/order.rb
+++ lib/order.rb
@@ -1,9 +1,11 @@
 require_relative 'order.rb'
 
 class Order
-    attr_reader :id, :products,
+    attr_reader :id
+    attr_accessor :products,:customer,:fufillment_status
 
-def initialize(id,products,customer,fufillment_status = pending)
+
+def initialize(id,products,customer,fufillment_status = :pending)
     @id = id 
     @products = products
     @customer = customer
@@ -11,15 +13,18 @@ def initialize(id,products,customer,fufillment_status = pending)
 end 
 
 def total
-price * .075%
-Rounding the result to two decimal places
+price * 0.075
 end
 
-def add_product(products_name,price)
-    products = {}
-    
+def add_product(product_name,product_price)
+    if products.has_key?product_name
+    return false
+    else @products[product_name] =
+    product_price 
+    return true
 end
-def fufillment_status
-    raise ArgumentError if fufillment_status 
+end
+def fufillment_status(fufillment_status)
+    raise ArgumentError if fufillment_status != :pending||:paid ||:processing ||:shipped||:complete
 end 
 end
