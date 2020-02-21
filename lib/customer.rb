9afe0790f561459abc5558cc01345e78dd46fdae
Nataliya Pogodina <n.pogodina@gmail.com>
2020-02-21 14:32:34 -0800

    Added Customer.all and Customer.find(id) methods.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -6,5 +8,32 @@ class Customer
     @id = id
     @email = email
     @address = address
+  end
+
+  def self.all
+    data = CSV.read("./data/customers.csv")
+    customer_info = []
+
+    data.each do |row|
+      id = row[0].to_i
+      email = row[1]
+      address = {}
+      address[:street] = row[2]
+      address[:city] = row[3]
+      address[:state] = row[4]
+      address[:zip] = row[5]
+      
+      customer = Customer.new(id, email, address)
+      customer_info << customer
+    end
+
+    return customer_info
+  end
+
+  def self.find(id)
+    customer_info = self.all
+    customer_with_id = customer_info.find { |customer| customer.id == id}
+    raise ArgumentError.new("No customer with ID #{id} found.") if customer_with_id == nil 
+    return customer_with_id
   end
 end
