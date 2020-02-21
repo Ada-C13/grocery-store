8327ebc6bc0fc16212ca7eda5fcfb19dc1bcb2a0
vwhwang <vwhwang38@gmail.com>
2020-02-21 12:24:19 -0800

    wave 2 Customer class works

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,12 +14,26 @@ class Customer
 
     def self.all
       dictionary_path = File.join(File.dirname(__FILE__),"../data/customers.csv")
-      keys = [:id,:email,:address,:city,:state,:zip]
       data = CSV.read(dictionary_path) 
-      data_with_header = data.map {|array| Hash[keys.zip(array)]}
-      customer_test = data_with_header.map {|hash| hash.slice(:id,:email,:address)}
-      p customer_test.class 
-      p customer_test[0]
+      list = []
+      data.each do |line|
+        hash_temp = {:street => line[2], :city => line[3], :state => line[4], :zip => line[5]}
+        list << Customer.new(line[0].to_i,line[1],hash_temp)
+      end 
+      return list
     end 
 
+    def self.find(id)
+      data = Customer.all
+      data.each do |line|
+        if line.id == id 
+          return line
+        end 
+      end 
+      return nil
+    end 
+
+
 end 
+
+
