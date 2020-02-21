f22a4f5ddb968c401b7d15dcec51bf420cb14df5
saraleecodes <sara.lee.higgins@gmail.com>
2020-02-20 17:01:09 -0800

    Wave 1: Raise argument error if fulfillment status is not valid

--- lib/order.rb
+++ lib/order.rb
@@ -7,5 +7,12 @@ class Order
 		@products = products
 		@customer = customer
 		@fulfillment_status = fulfillment_status
+		
+		valid_statuses = [:pending, :paid, :processing, :shipped, :complete]
+		raise ArgumentError.new("The fulfillment status is not valid!") if !valid_statuses.include?(@fulfillment_status)
+	end
+
+	def fulfillment_status
+		return @fulfillment_status
 	end
 end
