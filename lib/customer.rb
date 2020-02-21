1fd07263d5d8de128136acdb82b2875be60d6e8d
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-02-21 15:35:55 -0800

    passing 4 test in Wave 2

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,8 +13,23 @@ class Customer
   end
 
   def self.all
-    customers.csv = CSV.read(data / customers.csv)
+    customers = CSV.read('data/customers.csv')
+    list = customers.map do |customer|
+      id = customer[0].to_i
+      email = customer[1]
+      street = customer[2]
+      city = customer[3]
+      state = customer[4]
+      zip = customer[5]
+      customer_record = Customer.new(id,email,{:street=> street, :city => city, :state => state, :zip => zip})
+    end
+    return list
   end
 
-  def self.find(id); end
-end
+  def self.find(id)
+    list_of_customers = Customer.all
+    list_of_customers.each do|customer|
+    customer.id
+    end 
+  end
+  end
