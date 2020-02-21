75a2d5137a55b86f36a444b0a9109a1503080158
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-02-20 21:38:17 -0800

    fixed formatting in add_product/order.rb

--- lib/order.rb
+++ lib/order.rb
@@ -17,13 +17,14 @@ price * 0.075
 end
 
 def add_product(product_name,product_price)
-    if products.has_key?product_name
-    return false
-    else @products[product_name] =
-    product_price 
-    return true
+  if products.has_key?product_name
+   return false
+  else @products[product_name] =
+   product_price 
+  return true
 end
 end
+
 def fufillment_status(fufillment_status)
     raise ArgumentError if fufillment_status != :pending||:paid ||:processing ||:shipped||:complete
 end 
