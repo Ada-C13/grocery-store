14de19c74acb83486c2cc4ab4eaea744e4ad3f5c
Jetmire Bajrami <jetmirebajrami@Jetmires-MBP.localdomain>
2020-02-21 14:58:30 -0800

    Add an new method fine id

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
  attr_reader :id, :email, :address
  attr_writer :email, :address
@@ -5,6 +7,24 @@ class Customer
     @id = id
     @email = email
     @address = address
-  end   
-end
+  end  
 
+  
+  def self.all
+    customers = []
+    CSV.read('./data/customers.csv').each do |customer|
+      customers << Customer.new(customer[0].to_i, customer[1], {:street =>customer[2],:city =>customer[3],:state =>customer[4],:zip =>customer[5]})
+    end
+    return customers
+  end
+
+  def self.find(id)
+    Customer.all.each do |custumer|
+      if custumer.id  == id
+        return custumer
+      end
+    end
+    return nil
+  end
+
+end
