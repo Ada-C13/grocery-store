df200c6c33473a1b5d3b5d272ed7eeccf897777f
Jessica Stone <jstone4815@gmail.com>
2020-02-21 14:46:07 -0800

    pass tests for .all method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,9 @@
+require 'csv'
+customer_data = CSV.read('data/customers.csv')
+
+
 class Customer
-  attr_reader :id
+  attr_reader :id, :all_customers
   attr_accessor :email, :address
 
   def initialize(id, email, address)
@@ -7,4 +11,28 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    all_customers = []
+
+    CSV.read('data/customers.csv').each do |customer|
+      customer_address = {}
+  
+      customer_id = customer[0].to_i
+      customer_email = customer[1]
+      customer_address[:street] = customer[2]
+      customer_address[:city] = customer[3]
+      customer_address[:state] = customer[4]
+      customer_address[:zip] = customer[5]
+
+      all_customers << Customer.new(customer_id, customer_email, customer_address)
+    end
+    
+    return all_customers
+  end
+
+  def find(id)
+    Customer.all 
+  end
 end
+
