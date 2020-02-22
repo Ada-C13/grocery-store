cf0fb99606105cee5b42f298f9999a4a82e6051c
RebeccaRoach <rebeccaroach12@gmail.com>
2020-02-22 12:02:39 -0800

    Wave 2 tests passing

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -7,4 +9,29 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    all_customers = []
+    CSV.read('data/customers.csv').each do |customer|
+      id = customer[0].to_i
+      email = customer[1]
+      address = {}
+      address[:street] = customer[2]
+      address[:city] = customer[3]
+      address[:state] = customer[4]
+      address[:zip] = customer[5]
+      all_customers << Customer.new(id, email, address)
+    end
+    return all_customers
+  end
+
+  def self.find(id)
+    Customer.all.each do |customer|
+      if customer.id == id
+        return customer
+      end
+    end
+    return nil
+  end
+
 end
