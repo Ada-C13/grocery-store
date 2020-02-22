3683b77016c67acfcc415a7430e01b2ce309ed8b
SuelyBarreto <suely@barreto.us>
2020-02-21 20:53:47 -0800

    wave 2 customers

--- lib/customer.rb
+++ lib/customer.rb
@@ -4,6 +4,7 @@
 # Author : Suely Barreto
 # Date   : February 2020
 # 
+require 'csv'
 
 class Customer
 
@@ -16,7 +17,33 @@ class Customer
     @address = address
   end
 
-  
+  def self.all
+    filename = "data/customers.csv"
+    all_customers = []
+    csv_all = CSV.read(filename)
+
+    csv_all.each do |csv_row|
+
+      id    = csv_row[0].to_i
+      email = csv_row[1]
+      address = {
+        street: csv_row[2],
+        city: csv_row[3],
+        state: csv_row[4],
+        zip: csv_row[5]
+      }
+
+      customer = Customer.new(id, email, address)
+      all_customers << customer
+
+    end
+    return all_customers
+  end
+
+  def self.find(id)
+    return Customer.all.select { |c| c.id == id }.first
+  end
 
 end
+
 
