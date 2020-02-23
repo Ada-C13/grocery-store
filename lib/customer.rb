141d06b1d40d7236bdffd92894bfe5681f58c4ad
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-02-23 14:32:06 -0800

    wave 2 passed the test

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -8,4 +10,25 @@ class Customer
     @email = email
     @address = address
   end 
+
+  def self.all 
+    all_customer = []
+     CSV.read('./data/customers.csv').each do |customer| 
+      all_customer << Customer.new(customer[0].to_i, customer[1], {
+                  :street => customer[2], 
+                  :city => customer[3],
+                  :state => customer[4],
+                  :zip => customer[5]
+                })  
+    end 
+    return all_customer
+  end 
+
+  def self.find(id_customer)
+    id_find = all.find do |customer|
+      id_customer == customer.id
+    end 
+    return id_find
+  end 
+
 end 
--- lib/order.rb
+++ lib/order.rb
@@ -1,27 +1,47 @@
+require_relative 'customer'
 class Order 
   attr_reader :id 
   attr_accessor :products, :customer, :fulfillment_status
+  FULFILLMENT_VALID_STATUS = [:paid, :processing, :shipped, :complete, :pending].freeze
   def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id 
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
-    fulfillment_valid_status = [:paid, :processing, :shipped, :complete, :pending]
-    raise ArgumentError if !fulfillment_valid_status.include?(fulfillment_status)
+    raise ArgumentError if !FULFILLMENT_VALID_STATUS.include?(fulfillment_status)
   end 
+  
   def total
     tax = 0.075
     sum_values = products.values.sum 
     total_price = sum_values + (sum_values * tax)
     return total_price.round(2)
   end 
-
+  
   def add_product(name, price)
     raise ArgumentError if products.key?(name)
-    products[name] = price
+    products[name] = price.to_f.round(2)
   end
-
-
+  
+  def self.all
+    orders_info = []
+    CSV.read('./data/orders.csv').each do |order|
+      current_order = Order.new(order[0].to_i, {}, Customer.find(order[2].to_i), order[3].to_sym)
+      purchases = order[1].split(';')
+      purchases.each do |purchase|
+        name_price = purchase.split(':')
+        current_order.add_product(name_price[0], name_price[1])
+      end
+      orders_info << current_order
+    end 
+    return orders_info
+  end 
+  
+  def self.find(purchase_id)
+    id_find = all.find do |purchase|
+      purchase_id == purchase.id  
+    end 
+    return id_find
+  end 
+  
 end 
-
-
