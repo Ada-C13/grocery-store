77c49311f926d79d8497477457276e4b1fe90065
Hannah Tekie <hannahtekie@gmail.com>
2020-02-22 21:33:21 -0800

    wave 2 completed w/o the optionals and wave 3

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,6 +1,7 @@
 #each class should get it's own file (eg. lib/class_name.rb)
 #WAVE 1 (1)create a class, 2)write instance methods inside a class to perform actions, 3)use two classes using composition,
 #4) use exceptions to hadle erros, 5) test
+require "csv"
 
 class Customer
   attr_reader :id
@@ -11,18 +12,31 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    customer_instances = []
+    CSV.readlines("data/customers.csv", "r").each do |data|
+      #   print data[0]
+      id = data[0]
+      email = data[1]
+      address = {
+        :street => data[2],
+        :city => data[3],
+        :state => data[4],
+        :zip => data[5],
+      }
+      customer_instances << Customer.new(id.to_i, email, address)
+    end
+    return customer_instances
+  end
+
+  def self.find(id)
+    customer_instances = self.all #array of cxs
+    customer_instances.find do |customer_instance|
+      # <meta data> = customer_instances.id
+      if id == customer_instance.id
+        return customer_instance
+      end
+    end
+  end
 end
-
-# x = "hi"
-
-# x = "hello"
-# x = 'goodbye'
-
-# customer = customer.new(id, email,delivery_address)
-# fulfillment_status = fulfillment_status.new(:pending, :paid, :processing, :shipped, :complete)
-# fulfillment_status = fulfillment_status(:pending)
-
-# ben = Customer.new(10, 'ben@mail.com', {address})
-# hannah = Customer.new(11, 'hannah')
-# ben.orders
-# hannah.orders
--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,5 @@
+require_relative "customer"
+
 class Order
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
@@ -39,6 +41,64 @@ class Order
       products[product_name] = price
     end
   end
+
+  def self.all
+    order_instances = []
+
+    CSV.open("data/orders.csv", "r").each do |data|
+      id = data[0].to_i
+      products = data[1]  # "Lobster:17.18;Annatto seed:58.38;Camomile:83.21"
+      products_array = data[1].split(";") # ["Lobster:17.18", 'Annatto seed:58.38", "Camomile:83.21"]
+
+      products_hash = {}
+
+      products_array.each do |product_data| # "Lobster:17.18"
+        product = product_data.split(":") # ["Lobster", "17.18"]
+        product_name = product[0]
+        product_price = product[1].to_f
+
+        #hash[key] = value
+        products_hash[product_name] = product_price
+      end
+
+      customer_id = data[2].to_i
+
+      customer_instance = Customer.find(customer_id)
+
+      status = data[3].to_sym
+
+      order_instances << Order.new(id, products_hash, customer_instance, status)
+    end
+    return order_instances
+  end
+
+  def self.find(id)
+    order_instances = self.all
+    order_instance = order_instances.find do |order_instance|
+      id == order_instance.id
+    end
+
+    return order_instance
+  end
 end
 
-# Order.new(1,{}, ben, :error) # ArgumentError
+
+
+
+# AS IS
+# order_instances.find do |order_instance|
+
+#   if id == order_instance.id
+#     return order_instance
+#   end
+# end
+
+
+# TO BE
+# order_instance = order_instances.find do |order_instance|
+#   id == order_instance.id
+# end
+
+# return order_instance
+# end
+
