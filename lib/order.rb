fafecc11f159fa6a99cc127309cbe7434d320863
dnguye2 <dnguyen1093@gmail.com>
2020-02-22 16:57:16 -0800

    wave 2 order code and tests finished

--- lib/order.rb
+++ lib/order.rb
@@ -1,15 +1,16 @@
 require_relative 'customer'
+require 'csv'
 class Order
 
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
   def initialize(id, products, customer, fulfillment_status = :pending)
-    @id = id
+    @id = id.to_i
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
 
-    # takes care of fulfillment status
+    # Takes care of fulfillment status
     valid_statuses = [:pending, :paid, :processing, :shipped, :complete]
 
     unless valid_statuses.include?(fulfillment_status)
@@ -18,7 +19,7 @@ class Order
     
   end
 
-  # method to define total of product
+  # Method to define total of product
   def total
     total = 0
     @products.each_value do |value|
@@ -43,6 +44,34 @@ class Order
     end
   end
 
- 
+  # Wave 2
+
+  # Helper method for turning orders CSV of products
+  # from "name:price;nextname:nextprice" to hash
+  def self.orders_csv_product(product_string)
+    product_hash = Hash[*product_string.split(/[:;]/)]
+    product_hash = product_hash.each {|product_name, price| product_hash[product_name] = price.to_f}
+    return product_hash
+  end
+
+  # Method for that returns a collection of Order instances
+  def self.all
+    orders_csv = CSV.read("./data/orders.csv")
+
+    orders = orders_csv.map do |order_row|
+      Order.new(order_row[0],
+        orders_csv_product((order_row[1])), Customer.find((order_row[2]).to_i), (order_row[3]).to_sym)
+    end
+
+    return orders
+  end
+
+  # Method that returns Order
+  # when id given is found in CSV 
+  def self.find(id)
+    orders = Order.all
+    found_order = orders.detect {|order| (order.id) == id}
+    return found_order
+  end
 
 end
