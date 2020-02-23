3a32ead319d18256b34eedfcdd02f4c4c27f96c6
Catherina <catherine.pchelina@gmail.com>
2020-02-22 18:38:31 -0800

    Wave 3 optional: completed

--- lib/customer.rb
+++ lib/customer.rb
@@ -45,4 +45,18 @@ class Customer
 
     return nil
   end
+
+  def self.save(filename, new_customer)
+    CSV.open(filename, "a", headers: true) do |csv|
+      new_customer = [
+        new_customer.id, 
+        new_customer.email, 
+        new_customer.address[:street], 
+        new_customer.address[:city],
+        new_customer.address[:state], 
+        new_customer.address[:zip]
+      ]
+      csv << new_customer
+    end
+  end
 end
