a7dd125b981dec7df922b5970b0a1e0cdc62967f
Haben <haben7@gmail.com>
2020-02-22 17:20:15 -0800

    attempt at wave 3

--- lib/customer.rb
+++ lib/customer.rb
@@ -4,20 +4,20 @@ require "awesome_print"
 # create a customer class
 class Customer
   attr_reader :id
-  attr_accessor :email, :address # how do we know ???
+  attr_accessor :email, :address 
 
   def initialize(id, email, address) # The initialize creates customer list
     @id = id
-    @email = email # going to be in a string
-    @address = address # going to be in a hash
+    @email = email # string
+    @address = address # hash
   end
 
   # Wave 2
   def self.all
-    # returns a collection of Customer instances, representing all of the Customer described in the CSV file
+    # returns a collection of Customer instances, representing all of the Customers described in the CSV file
     allcustomers = CSV.read("data/customers.csv").map(&:to_a)
     customers = []
-    # iterating through the allcustomers array per customer, or element in that array
+    # iterate through the allcustomers array 
     allcustomers.each do |customer_data|
       # taking each customer and extracting id at index 0
       customers << Customer.new(customer_data[0].to_i, customer_data[1], customer_data[2..5]) # each customer object here will be an instance of that customers array
@@ -36,8 +36,19 @@ class Customer
     return customers    
   end
 
-  # self.find(id) - returns an instance of Customer where the value of the id field in the CSV matches the passed parameter
+  # return an instance of Customer where the value of the id field in the CSV matches the passed parameter
   def self.find(id)
     Customer.all.find { |customer| customer.id == id }
   end
 end
+
+
+#  # wave 3
+#   def save(filename, new_customer)
+#     CSV.open("data/customers.csv", "a+") do |csv|
+#       csv << new_customer
+#     end
+#   end not finished
+
+# 1,leonard.rogahn@hagenes.org,71596 Eden Route,Connellymouth,LA,98872-9105
+
