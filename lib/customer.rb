fa62e2674ab9f3cc49a308de72fe6a6b8e22cc3c
Yaz <yasmineoshaugh@gmail.com>
2020-02-22 22:15:25 -0800

    added and tested class methods all and find for Customer.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'ap'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -8,4 +11,22 @@ class Customer
     @address = address
   end
 
+  def self.all
+    all_customers = []
+    customers = CSV.read('data/customers.csv')
+    customers.each do |customer|
+        address = {
+          street: customer[2],
+          city: customer[3],
+          state: customer[4],
+          zip: customer[5]
+        }
+        all_customers << Customer.new(customer[0].to_i, customer[1], address)
+    end
+    return all_customers
+  end
+
+  def self.find(id)
+    return Customer.all[id-1]
+  end
 end
