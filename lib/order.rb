bdf96675ceb29fb9a429f0c5c2170e87f0252b6a
Nataliya Pogodina <n.pogodina@gmail.com>
2020-02-23 15:39:38 -0800

    Added Order.all and Order.find(id) methods. Added tests for Order.find(id).

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,6 @@
+require "csv"
+require_relative "customer.rb"
+
 class Order
   attr_reader :id
   attr_accessor :products, :fulfillment_status, :customer
@@ -24,4 +27,48 @@ class Order
     @products[name] = price 
     return @products 
   end
+
+  def self.all
+    data = CSV.read("./data/orders.csv")
+    order_info = []
+
+    data.each do |row|
+      id = row[0].to_i
+      customer = Customer.find(row[2].to_i)
+      fulfillment_status = get_fulfillment_status(row[3])
+      products = get_products(row[1])
+
+      order = Order.new(id, products, customer, fulfillment_status)
+      order_info << order
+    end
+    return order_info
+  end
+
+  def self.find(id)
+    order_info = self.all
+    order_with_id = order_info.find { |order| order.id == id}
+    return order_with_id
+  end
+end
+
+# HELPER METHODS
+def get_fulfillment_status(string_status)
+  status_data = {
+    "pending" => :pending,
+    "paid" => :paid,
+    "processing" => :processing,
+    "shipped" => :shipped,
+    "complete" => :complete
+  }
+  fulfillment_status = status_data[string_status]
+  return fulfillment_status
+end
+
+def get_products(products_data)
+  products = {}
+  products_data.split(";").each do |product_price_pair|
+    product_price_pair_array = product_price_pair.split(":")
+    products[product_price_pair_array[0]] = product_price_pair_array[1].to_f
+  end 
+  return products
 end
