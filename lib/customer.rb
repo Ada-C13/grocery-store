40005dc7babb99efe7f3b3f5e38ef5909a1b4c9a
Katie <katie.vandervoort@gmail.com>
2020-02-23 23:50:20 -0800

    cleanup and added comments for clarity

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,6 @@
 require 'csv'
 
 class Customer
-  
   attr_reader :id
   attr_accessor :email, :address
 
@@ -11,29 +10,19 @@ class Customer
     @address = address # a hash
   end
 
+  # class method which reads the customers CSV and returns a collection of those Customer instances
   def self.all
     collection_of_customers = []
     CSV.read("data/customers.csv").map(&:to_a).each do |customer|
       collection_of_customers.push(Customer.new(customer[0].to_i,customer[1],{street: customer[2], city: customer[3], state: customer[4], zip: customer[5]}))
     end
-    # print collection_of_customers[0].email
-    # print "\n"
     return collection_of_customers
   end
-# self.find(id) - returns an instance of Customer where the value of the id field in the CSV matches the passed parameter
-# Customer.find should not parse the CSV file itself. 
-# Instead it should invoke Customer.all and search through the results for a customer with a matching ID
 
+  # class method which searches for and returns a customer from data in the CSV file based on the customer ID input
+ 
   def self.find(id)
-    # self.all.each do |customer|
-    #   if customer.id == id
-    #     return customer
-    #   end
-    # end
-    # return nil
     return self.all.find { |customer| customer.id == id }
   end
 
 end
-
-# puts "#{Customer.find(3).email}"
