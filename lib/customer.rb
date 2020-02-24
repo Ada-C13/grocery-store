00ebba0ac4bf84d99f071ab42fe8e733923d7009
saraleecodes <sara.lee.higgins@gmail.com>
2020-02-23 23:56:03 -0800

    Wave 2: Define self.all method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
 	attr_accessor :email, :address
 	attr_reader :id
@@ -7,4 +9,25 @@ class Customer
 		@email = email
 		@address = address
 	end
+
+	# self.all - returns a collection of Customer instances, representing all of the Customer described in the CSV file
+	def self.all
+		CSV.read('data/customers.csv', converters: :all).map do |customer|
+			Customer.new(
+				customer[0],
+				customer[1],
+				{ 
+				street: customer[2],
+				city: customer[3],
+				state: customer[4],
+				zip: customer[5]
+				}
+			)
+		end
+	end
+
+	# self.find(id) - returns an instance of Customer where the value of the id field in the CSV matches the passed parameter
+
+	# Customer.find should not parse the CSV file itself. Instead it should invoke Customer.all and search through the results for a customer with a matching ID.
+
 end
