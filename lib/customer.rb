c152a3ca5eb2cd067b74537feca752e015ea9784
Lola Llanes <mslourdesllanes@gmail.com>
2020-02-25 10:23:11 -0800

    final customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,27 +1,31 @@
+require "csv"
+
 class Customer
 
-    attr_reader :ID
-    attr_accessor :email_address, :delivery_address
+    attr_reader :id, :email_address, :delivery_address
 
-
-    def initalize(ID, email_address, delivery_Address)
-        @ID = ID
+    def initialize(id, email_address, delivery_address)
+        @id = id
         @email_address = email_address
         @delivery_address = delivery_address
     end
 
 
+    def self.all
+      customers = []
+      CSV.open("data/customers.csv", "r") do |file|
+        file.each do |row|
+          address = {}
+          address[:street] = row[2]
+          address[:city] = row[3]
+          address[:state] = row[4]
+          address[:zip] = row[5]
+          customer = Customer.new(row[0].to_i, row[1], address)
+          customers << customer
+        end
+      end
+      return customers
+    end
 
+ end
 
-
-
-
-
-
-
-
-
-
-
-
-end
